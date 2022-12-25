1e2b16d8ad3247334445dc144d259bb416e4b1de
Kanaknakola <jakub.pecak@o2.pl>
2022-12-25 11:30:45 +0100

    loader

--- src/app/services/loader.service.ts
+++ src/app/services/loader.service.ts
@@ -5,6 +5,16 @@ import { Injectable } from '@angular/core';
 })
 export class LoaderService {
 
- 
+  private loading: boolean = false;
 
+  constructor() { }
+
+  setLoading(loading: boolean) {
+    this.loading = loading;
+  }
+
+  getLoading(): boolean {
+    return this.loading;
+  }
+  
 }
