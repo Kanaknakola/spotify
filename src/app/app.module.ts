00429bf492201080838dc461431c3e9a0696aa33
Kanaknakola <jakub.pecak@o2.pl>
2022-12-25 11:45:34 +0100

    added global spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { LoadingInterceptor } from './interceptor-loading/loading.interceptor';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/spinner/spinner.component.ts
@@ -0,0 +1,16 @@
+import { LoaderService } from './../services/loader.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss']
+})
+export class SpinnerComponent implements OnInit {
+
+  constructor(public loader: LoaderService) { }
+
+  ngOnInit(): void {
+  }
+
+}
